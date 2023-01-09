bc4a7bee17bd743bae9edf2ce6e4d1d0edbb999b
Lucas Araújo <lucasarasouza@hotmail.com>
2023-01-09 20:23:10 -0300

    Adição da criação dinâmica de VNets

--- azure/two-vms-basic-architecture/modular/modules/virtual-network/main.tf
+++ azure/two-vms-basic-architecture/modular/modules/virtual-network/main.tf
@@ -1,3 +1,21 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  for_each            = var.vnets
+  name                = each.key
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  address_space       = [each.value.address_space]
+  dns_servers         = [lookup(each.value, "dns_servers", null)]
+  dynamic "subnet" {
+    for_each = lookup(each.value, "subnet", {}) != {} ? [1] : []
+    content {
+      name           = lookup(each.value.subnet, "name", null)
+      address_prefix = lookup(each.value.subnet, "address_prefix", null)
+      security_group = lookup(each.value.subnet, "security_group", null)
+    }
+  }
+  depends_on = [data.azurerm_resource_group.rg]
+}
