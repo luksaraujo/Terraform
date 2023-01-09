171a392661c9d5752976a41ae46170e7b5d7d4bd
Lucas Araújo <lucasarasouza@hotmail.com>
2023-01-09 20:32:22 -0300

    Adição da variável que conterá as VNets

--- azure/two-vms-basic-architecture/modular/modules/virtual-network/variables.tf
+++ azure/two-vms-basic-architecture/modular/modules/virtual-network/variables.tf
@@ -2,3 +2,8 @@ variable "resource_group_name" {
   type        = string
   description = "Nome do Resource Group que será importado"
 }
+
+variable "vnets" {
+  type        = any
+  description = "Virtual Networks que serão criadas, juntamente com suas subnets"
+}
