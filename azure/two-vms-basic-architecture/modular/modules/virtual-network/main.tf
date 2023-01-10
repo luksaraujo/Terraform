429354ec2899ff41e757538ffe9a42d698669ef4
Lucas Araújo <lucasarasouza@hotmail.com>
2023-01-09 21:17:13 -0300

    Alteração da estratégia de nomeação dos DNS Servers

--- azure/two-vms-basic-architecture/modular/modules/virtual-network/main.tf
+++ azure/two-vms-basic-architecture/modular/modules/virtual-network/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   address_space       = [each.value.address_space]
-  dns_servers         = [lookup(each.value, "dns_servers", null)]
+  dns_servers         = lookup(each.value, "dns_servers", null)
   tags                = lookup(each.value, "tags", data.azurerm_resource_group.rg.tags)
   dynamic "subnet" {
     for_each = lookup(each.value, "subnet", {}) != {} ? [1] : []
