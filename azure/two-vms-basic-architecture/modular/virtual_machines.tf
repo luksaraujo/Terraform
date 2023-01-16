1c6cadbbf5040be421bad1f41f1ae579e237f1fc
Lucas Araújo <lucasarasouza@hotmail.com>
2023-01-16 18:50:18 -0300

    Adicionando códigos da solução proposta

--- azure/two-vms-basic-architecture/modular/virtual_machines.tf
+++ azure/two-vms-basic-architecture/modular/virtual_machines.tf
@@ -1,55 +1,5 @@
-module "vm-windows-sequencial" {
-  source                        = "./modules/virtual-machine"
-  resource_group_name           = azurerm_resource_group.rg-01.name
-  estrategia_de_implementacao   = "sequencial"
-  vnet_name                     = module.vnet.vnet_name
-  sequencial_identical_vm_names = "vm-win-lab"
-  sequencial_qtde_vms           = 1
-  os_vms                        = "Windows"
-  vm_size                       = "Standard_B1ms"
-  admin_username                = "lucas"
-  admin_password                = "P@assw0rd"
-  subnet_name                   = "lab-lucas-souza-tf-simple-infra-sn01"
-  depends_on                    = [module.vnet]
-  os_disk = {
-    os_disk_caching              = "ReadWrite"
-    os_disk_storage_account_type = "Standard_LRS"
-  }
-  os_disk_source_image_reference = {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-}
-
-module "vm-linux-sequencial" {
-  source                        = "./modules/virtual-machine"
-  resource_group_name           = azurerm_resource_group.rg-01.name
-  estrategia_de_implementacao   = "sequencial"
-  vnet_name                     = module.vnet.vnet_name
-  sequencial_identical_vm_names = "vm-lnx-lab"
-  sequencial_qtde_vms           = 1
-  os_vms                        = "Linux"
-  vm_size                       = "Standard_B1ms"
-  admin_username                = "lucas"
-  admin_password                = "P@assw0rd"
-  subnet_name                   = "lab-lucas-souza-tf-simple-infra-sn01"
-  depends_on                    = [module.vnet]
-  os_disk = {
-    os_disk_caching              = "ReadWrite"
-    os_disk_storage_account_type = "Standard_LRS"
-  }
-  os_disk_source_image_reference = {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-}
-
 module "vm-windows-dinamico" {
-  source                      = "./modules/virtual-machine"
+  source                      = "../../modules/virtual-machine"
   resource_group_name         = azurerm_resource_group.rg-01.name
   estrategia_de_implementacao = "dinamico"
   vnet_name                   = module.vnet.vnet_name
@@ -57,13 +7,12 @@ module "vm-windows-dinamico" {
   os_vms                      = "Windows"
   depends_on                  = [module.vnet]
   vms_dynamic = {
-    "vm-frontend" = { size = "Standard_B1ms", admin_username = "lukita", admin_password = var.admin_password },
-    "vm-backend"  = { size = "Standard_DS1_v2", admin_username = "lucas", admin_password = var.admin_password }
+    "vm-windows-01" = { size = "Standard_B1ms", admin_username = "lukita", admin_password = var.admin_password }
   }
 }
 
 module "vm-linux-dinamico" {
-  source                      = "./modules/virtual-machine"
+  source                      = "../../modules/virtual-machine"
   resource_group_name         = azurerm_resource_group.rg-01.name
   estrategia_de_implementacao = "dinamico"
   vnet_name                   = module.vnet.vnet_name
@@ -71,7 +20,6 @@ module "vm-linux-dinamico" {
   os_vms                      = "Linux"
   depends_on                  = [module.vnet]
   vms_dynamic = {
-    "vm-fe-lnx" = { size = "Standard_B1ms", admin_username = "lukita", admin_password = var.admin_password },
-    "vm-be-lnx"  = { size = "Standard_DS1_v2", admin_username = "lucas", admin_password = var.admin_password }
+    "vm-linux-01" = { size = "Standard_B1ms", admin_username = "lukita", admin_password = var.admin_password }
   }
 }
